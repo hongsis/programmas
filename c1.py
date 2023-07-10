3c3543c43aa603157cbb6e1f7c6212410618b7d6
hongsis <subinn2584@gmail.com>
2023-07-10 20:28:44 +0900

    c2

--- c1.py
+++ c1.py
@@ -2,3 +2,13 @@ def solution(x):
     answer = 0
     answer = int(x[0]) + int(x[-1])
     return answer
+
+def solution(L, x):
+    answer = []
+    for i, v in enumerate(L):
+        if x == v :
+            answer.append(i)
+
+    if answer == []:
+        answer.append(-1)
+    return answer
